2d1388f7f6b3840c7b16fd9f5d002d80d156e646
louisjclu <louisjclu@gmail.com>
2021-02-06 09:23:44 +0800

    day4

---
+++ d4.py
@@ -0,0 +1,8 @@
+import numpy as np
+english_score= np.array([55,89,76,65,48,70])
+math_score = np.array([60,85,60,68,55,60])
+chinese_score = np.array([65,90,82,72,66,77])
+
+print(np.greater(english_score, math_score))
+
+print(np.all(np.greater(chinese_score , [english_score, math_score])))
